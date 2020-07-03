80d94d37c10b5db18d8426da1d9366dde4813cad
Nicola Pedrocchi <nicola.pedrocchi@stiima.cnr.it>
2020-07-03 16:58:11 +0200

    nicola: codecov

--- .coverage.sh
+++ .coverage.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-
-#!/bin/bash
 # Export CI environment info: https://github.com/mikeferguson/code_coverage
 
-cd ~/catkin_ws
+echo "Total Arguments:" $#
+echo "All Arguments values:" $@
+
+cd $1
 
 export PROJECT_NAME="cnr_logger"
 export CODECOV_TOKEN="dc6c3b82-47d4-4d2d-99a4-0aa76a4640ef"
@@ -16,7 +17,17 @@ catkin_make -DENABLE_COVERAGE_TESTING=ON -DCMAKE_BUILD_TYPE=Debug $PROJECT_NAME_
 echo "Uploading coverage results to codecov.io"
 
 # Remove duplicated information
-rm "build/$PROJECT_NAME/robot_calibration_coverage_report.info.cleaned"
-rm "build/$PROJECT_NAME/robot_calibration_coverage_report.info.removed"
+if [ -f "build/$PROJECT_NAME/"$PROJECT_NAME"_coverage_report.info.cleaned" ]; then
+    rm "build/$PROJECT_NAME/"$PROJECT_NAME"_coverage_report.info.cleaned"
+fi
+if [ -f "build/$PROJECT_NAME/"$PROJECT_NAME"_coverage_report.info.removed" ]; then
+    rm "build/$PROJECT_NAME/"$PROJECT_NAME"_coverage_report.info.removed"
+fi 
 
-./.codecov -s "build/$PROJECT_NAME/" -t $CODECOV_TOKEN
+echo "==================================================================="
+CODECOV_WORKING_DIR="build/$PROJECT_NAME/"$PROJECT_NAME"_coverage_report/codecov"
+mkdir -p $CODECOV_WORKING_DIR
+cd $CODECOV_WORKING_DIR
+source "$1/src/$PROJECT_NAME/.codecov" -s "$1/build/$PROJECT_NAME/" -t $CODECOV_TOKEN -X coveragepy
+cd $1
+echo "==================================================================="
