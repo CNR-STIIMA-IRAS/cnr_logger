1cfe94846295fe668a9eac999026aa3f48031884
Nicola Pedrocchi <nicola.pedrocchi@stiima.cnr.it>
2020-07-03 07:22:55 +0200

    nicola: rostest integrated

--- .codecov.sh
+++ .codecov.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
 # Export CI environment info
-ci_env=`bash <(curl -s https://codecov.io/env)`
-export DOCKER_RUN_OPTS="$DOCKER_RUN_OPTS $ci_env"
+
+cd ~/catkin_ws
+
+echo "Generating coverage for $PROJECT_NAME"
+
+export PROJECT_NAME_COVER_REPORT= $PACKAGE_NAME"_coverage_report"
+
+catkin_make -DENABLE_COVERAGE_TESTING=ON -DCMAKE_BUILD_TYPE=Debug $PROJECT_NAME_COVER_REPORT
+
+echo "Uploading coverage results to codecov.io"
+
+# Remove duplicated information
+rm "build/$PROJECT_NAME/robot_calibration_coverage_report.info.cleaned"
+rm "build/$PROJECT_NAME/robot_calibration_coverage_report.info.removed"
+
+# Actually upload coverage information
+bash <(curl -s https://codecov.io/bash) -s "$ws/build/$PROJECT_NAME/"
+
+#curl -s https://codecov.io/bash > .codecov
+#chmod +x .codecov
+#./.codecov
+
+
+#ci_env=`bash <(curl -s https://codecov.io/env)`
+#export DOCKER_RUN_OPTS="$DOCKER_RUN_OPTS $ci_env"
--- .coverage.sh
+++ .coverage.sh
@@ -3,11 +3,9 @@
 source "${ICI_SRC_PATH}/workspace.sh"
 source "${ICI_SRC_PATH}/util.sh"
 
-echo "Generating coverage for robot_calibration"
+echo "Generating coverage for $PROJECT_NAME"
 
-ws=~/target_ws
-extend="/opt/ros/$ROS_DISTRO"
-ici_exec_in_workspace "$extend" "$ws" catkin build robot_calibration -v --no-deps --catkin-make-args robot_calibration_coverage_report
+catkin build $PROJECT_NAME -v --no-deps --catkin-make-args $PROJECT_NAME_coverage_report
 
 echo "Uploading coverage results to codecov.io"
 
