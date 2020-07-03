dcc033f3d41e3f870a348407709c17cc477f0604
Nicola Pedrocchi <nicola.pedrocchi@stiima.cnr.it>
2020-07-03 15:17:20 +0200

    nicola: codevov

--- .codecov.sh
+++ .codecov.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # Export CI environment info: https://github.com/mikeferguson/code_coverage
 
-cd ~/catkin_ws
+#cd ~/catkin_ws
 
+export PROJECT_NAME="cnr_logger"
+export CODECOV_TOKEN="dc6c3b82-47d4-4d2d-99a4-0aa76a4640ef"
 echo "Generating coverage for $PROJECT_NAME"
 
 export PROJECT_NAME_COVER_REPORT= $PACKAGE_NAME"_coverage_report"
@@ -16,10 +18,9 @@ rm "build/$PROJECT_NAME/robot_calibration_coverage_report.info.cleaned"
 rm "build/$PROJECT_NAME/robot_calibration_coverage_report.info.removed"
 
 # Actually upload coverage information
-#bash <(curl -s https://codecov.io/bash) -s "$ws/build/$PROJECT_NAME/"
+#bash <(curl -s https://codecov.io/bash) -s "build/$PROJECT_NAME/"
 
 curl -s https://codecov.io/bash > .codecov
 chmod +x .codecov
-./.codecov -s "$ws/build/$PROJECT_NAME/" -t %CODECOV_TOKEN
+./.codecov -s "build/$PROJECT_NAME/" -t $CODECOV_TOKEN
 
-+
