7dcb6e8bb86d41e9a81b934f031e99f0133f28c9
Nicola Pedrocchi <nicola.pedrocchi@stiima.cnr.it>
2020-07-05 04:25:43 +0200

    Nicola: codecov: bug

--- .coverage.sh
+++ .coverage.sh
@@ -20,19 +20,19 @@ catkin build -v --no-deps "$PROJECT_NAME" --catkin-make-args "$PROJECT_NAME_COVE
 echo "Uploading coverage results to codecov.io"
 
 # Remove duplicated information
-ls "build/$PROJECT_NAME/$PROJECT_NAME_COVER_REPORT.*"
-if [ -f "build/$PROJECT_NAME/$PROJECT_NAME_COVER_REPORT.info.cleaned" ]; then
-    rm "build/$PROJECT_NAME/$PROJECT_NAME_COVER_REPORT.info.cleaned"
+ls "$1/build/$PROJECT_NAME/$PROJECT_NAME_COVER_REPORT.*"
+if [ -f "$1/build/$PROJECT_NAME/$PROJECT_NAME_COVER_REPORT.info.cleaned" ]; then
+    rm "$1/build/$PROJECT_NAME/$PROJECT_NAME_COVER_REPORT.info.cleaned"
 fi
-if [ -f "build/$PROJECT_NAME/$PROJECT_NAME_COVER_REPORT.info.removed" ]; then
-    rm "build/$PROJECT_NAME/$PROJECT_NAME_COVER_REPORT.info.removed"
+if [ -f "$1/build/$PROJECT_NAME/$PROJECT_NAME_COVER_REPORT.info.removed" ]; then
+    rm "$1/build/$PROJECT_NAME/$PROJECT_NAME_COVER_REPORT.info.removed"
 fi
 
 echo "==================================================================="
-CODECOV_WORKING_DIR="build/$PROJECT_NAME/$PROJECT_NAME_coverage_report/codecov"
+CODECOV_WORKING_DIR="build/$PROJECT_NAME/$PROJECT_NAME_COVER_REPORT/codecov"
 mkdir -p "$CODECOV_WORKING_DIR"
 cd "$CODECOV_WORKING_DIR"
-"$1/src/$PROJECT_NAME/./.codecov" -s "$1/build/$PROJECT_NAME/" -t "$CODECOV_TOKEN" -X coveragepy
-echo "Result $?"
+bash <(curl -s https://codecov.io/bash) -t "$CODECOV_TOKEN" -s "$1/build/$PROJECT_NAME/" -X gcov -X coveragepy -X fix -q n.txt -Z -B master -C bb946ec4528b0bc83f76427bc124ef25447d014e
+"Result $?"
 cd "$CWD"
 echo "==================================================================="
