2dcd1aa7fca45f8c17e8f7ff370c13895de03f21
Nicola Pedrocchi <nicola.pedrocchi@stiima.cnr.it>
2020-07-11 11:06:32 +0200

    nicola: change .travis following the example from robot_calibration

---
+++ .coverage.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+source "${ICI_SRC_PATH}/workspace.sh"
+source "${ICI_SRC_PATH}/util.sh"
+
+echo "Generating coverage for cnr_logger"
+
+ws=~/target_ws
+extend="/opt/ros/$ROS_DISTRO"
+ici_exec_in_workspace "$extend" "$ws" catkin build cnr_logger -v --no-deps --catkin-make-args cnr_logger_coverage_report
+
+echo "Uploading coverage results to codecov.io"
+
+# Remove duplicated information
+rm "$ws/build/cnr_logger/cnr_logger_coverage_report.info.cleaned"
+rm "$ws/build/cnr_logger/cnr_logger_coverage_report.info.removed"
+
+# Actually upload coverage information
+bash <(curl -s https://codecov.io/bash) -s "$ws/build/cnr_logger/"
