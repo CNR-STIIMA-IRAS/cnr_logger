2ac28baf25c2033a8eaca59f5a7c4a74b4bdae78
Nicola Pedrocchi <nicola.pedrocchi@stiima.cnr.it>
2020-07-15 05:45:05 +0200

    nicola: bugfux init

--- test/test_cnr_logger.cpp
+++ test/test_cnr_logger.cpp
@@ -1,37 +1,37 @@
-/**
-  *  Software License Agreement (New BSD License)
-  *
-  *  Copyright 2020 National Council of Research of Italy (CNR)
-  *
-  *  All rights reserved.
-  *
-  *  Redistribution and use in source and binary forms, with or without
-  *  modification, are permitted provided that the following conditions
-  *  are met:
-  *
-  *   * Redistributions of source code must retain the above copyright
-  *     notice, this list of conditions and the following disclaimer.
-  *   * Redistributions in binary form must reproduce the above
-  *     copyright notice, this list of conditions and the following
-  *     disclaimer in the documentation and/or other materials provided
-  *     with the distribution.
-  *   * Neither the name of the copyright holder(s) nor the names of its
-  *     contributors may be used to endorse or promote products derived
-  *     from this software without specific prior written permission.
-  *
-  *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-  *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-  *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-  *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-  *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-  *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-  *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-  *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-  *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-  *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-  *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-  *  POSSIBILITY OF SUCH DAMAGE.
-  */
+/*
+ *  Software License Agreement (New BSD License)
+ *
+ *  Copyright 2020 National Council of Research of Italy (CNR)
+ *
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the copyright holder(s) nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #include <iostream>
 #include <ros/ros.h>
@@ -46,6 +46,8 @@ TEST(TestSuite, fullConstructor)
 {
 
   EXPECT_NO_FATAL_FAILURE(logger.reset(new cnr_logger::TraceLogger("log1", "/file_and_screen_different_appenders")));
+  EXPECT_FALSE(logger->init("/file_and_screen_different_appenders", false, false));  // Already initialized
+
   EXPECT_NO_FATAL_FAILURE(logger2.reset(new cnr_logger::TraceLogger("log2", "/file_and_screen_same_appender")));
 
   EXPECT_TRUE(logger->logFile());
@@ -63,14 +65,17 @@ TEST(TestSuite, fullConstructor)
 TEST(TestSuite, partialConstructor)
 {
   EXPECT_NO_FATAL_FAILURE(logger.reset(new cnr_logger::TraceLogger("log1")));
-  EXPECT_TRUE(logger->init("/file_and_screen_different_appenders"));
+  EXPECT_TRUE(logger->init("/file_and_screen_different_appenders", false, false));
+  EXPECT_FALSE(logger->init("/file_and_screen_different_appenders", false, false));  // Already initialized
   EXPECT_NO_FATAL_FAILURE(logger.reset());
 }
 
 TEST(TestSuite, wrongConstructor)
 {
   EXPECT_NO_FATAL_FAILURE(logger.reset(new cnr_logger::TraceLogger("log1")));
-  EXPECT_FALSE(logger->init("/this_namespace_does_not_exist"));
+  EXPECT_FALSE(logger->init("/this_namespace_does_not_exist", false, false));
+  EXPECT_TRUE( logger->init("/this_namespace_does_not_exist", false, true));  // default vaules depiste none parameters
+                                                                              // exits
   EXPECT_NO_FATAL_FAILURE(logger.reset());
 }
 
@@ -82,10 +87,25 @@ TEST(TestSuite, flushInfoDebug)
 
   for (size_t i = 0; i < 10; i++)
   {
-    CNR_INFO(*logger, "Ciao-log-1-info");
+    CNR_INFO( *logger, "Ciao-log-1-info");
     CNR_DEBUG(*logger, "Ciao-log-1-debug");
-    CNR_FATAL(*logger, "Ciao-log-1-debug");
-    CNR_TRACE(*logger, "Ciao-log-1-debug");
+    CNR_FATAL(*logger, "Ciao-log-1-fatal");
+    CNR_TRACE(*logger, "Ciao-log-1-trace");
+
+    CNR_INFO_THROTTLE(*logger,  1.0, "Ciao-log-1-info");
+    CNR_DEBUG_THROTTLE(*logger, 1.0, "Ciao-log-1-debug");
+    CNR_FATAL_THROTTLE(*logger, 1.0, "Ciao-log-1-fatal");
+    CNR_TRACE_THROTTLE(*logger, 1.0, "Ciao-log-1-trace");
+
+    CNR_INFO_COND( *logger, true , "Ciao-log-1-info");
+    CNR_DEBUG_COND(*logger, false, "Ciao-log-1debug");
+    CNR_FATAL_COND(*logger, true , "Ciao-log-1fatal");
+    CNR_TRACE_COND(*logger, false, "Ciao-log-1trace");
+
+    CNR_INFO_COND_THROTTLE( *logger, true , 1.0, "Ciao-log-1-info");
+    CNR_DEBUG_COND_THROTTLE(*logger, false, 1.0, "Ciao-log-1-debug");
+    CNR_FATAL_COND_THROTTLE(*logger, true , 1.0, "Ciao-log-1-fatal");
+    CNR_TRACE_COND_THROTTLE(*logger, false, 1.0, "Ciao-log-1-trace");
 
     CNR_INFO(*logger2, "Ciao-log-2-info");
     CNR_DEBUG(*logger2, "Ciao-log-2-debug");
