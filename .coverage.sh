6c86ccf9beb2d350f3e8309a04151eb02983dc20
Nicola Pedrocchi <nicola.pedrocchi@stiima.cnr.it>
2022-02-08 16:22:26 +0100

    code coverage integration with industrial_ci

--- .coverage.sh
+++ .coverage.sh
@@ -3,17 +3,17 @@
 source "${ICI_SRC_PATH}/workspace.sh"
 source "${ICI_SRC_PATH}/util.sh"
 
-echo "Generating coverage for 'cnr_logger'"
+echo "Generating coverage for 'rosparam_utilities'"
 
 ws=~/target_ws
 extend="/opt/ros/$ROS_DISTRO"
-ici_exec_in_workspace "$extend" "$ws" catkin build cnr_logger -v --no-deps --catkin-make-args coverage_report
+ici_exec_in_workspace "$extend" "$ws" catkin build rosparam_utilities -v --no-deps --catkin-make-args coverage_report
 
 echo "Uploading coverage results to codecov.io"
 
 # Remove duplicated information
-rm "$ws/build/cnr_logger/coverage_report.info.cleaned"
-rm "$ws/build/cnr_logger/coverage_report.info.removed"
+rm "$ws/build/rosparam_utilities/coverage_report.info.cleaned"
+rm "$ws/build/rosparam_utilities/coverage_report.info.removed"
 
 # Actually upload coverage information
-bash <(curl -s https://codecov.io/bash) -s "$ws/build/cnr_logger/"
+bash <(curl -s https://codecov.io/bash) -s "$ws/build/rosparam_utilities/"
