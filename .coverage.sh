e74356f44349b3ecf9fbac450b74de67bff809b3
Nicola Pedrocchi <nicola.pedrocchi@stiima.cnr.it>
2022-02-09 09:28:32 +0100

    nicola: simplify github action

--- .coverage.sh
+++ .coverage.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-source "${ICI_SRC_PATH}/workspace.sh"
-source "${ICI_SRC_PATH}/util.sh"
+# source "${ICI_SRC_PATH}/workspace.sh"
+# source "${ICI_SRC_PATH}/util.sh"
 
-echo "Generating coverage for 'cnr_logger'"
+# echo "Generating coverage for 'cnr_logger'"
 
-ws=~/target_ws
-extend="/opt/ros/$ROS_DISTRO"
-ici_exec_in_workspace "$extend" "$ws" catkin build cnr_logger -v --no-deps --catkin-make-args coverage_report
+# ws=~/target_ws
+# extend="/opt/ros/$ROS_DISTRO"
+# ici_exec_in_workspace "$extend" "$ws"
+catkin build cnr_logger -v --no-deps --catkin-make-args coverage_report
 
 echo "Uploading coverage results to codecov.io"
 
