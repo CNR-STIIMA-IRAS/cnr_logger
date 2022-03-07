15c66397a46ce0f669aed0ed6aa686802ad5f8c0
Nicola Pedrocchi <nicola.pedrocchi@stiima.cnr.it>
2022-03-07 17:26:30 +0100

    test - ci isolation

--- .coverage.sh
+++ .coverage.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-#ICI_SRC_PATH="/home/runner/target_ws/"
-
-#echo "ICI_SRC_PATH: ${ICI_SRC_PATH}$
-
 #source "${ICI_SRC_PATH}/workspace.sh"
 #source "${ICI_SRC_PATH}/util.sh"
 
@@ -12,6 +8,8 @@ echo "Generating coverage for 'cnr_logger'"
 ws=~/target_ws
 cd $ws
 
+catkin build cnr_logger --cmake-args -DCMAKE_BUILD_TYPE=Debug -DUSE_ROS=ON -DENABLE_TESTING=ON -DENABLE_COVERAGE_TESTING=ON
+catkin build cnr_logger --catkin-make-args run_tests
 catkin build cnr_logger -v --no-deps --catkin-make-args coverage_report
 
 echo "Uploading coverage results to codecov.io"
