ae3d1802b6cff1bcc45a5862e8e7ae2c831fd5b2
Nicola Pedrocchi <nicola.pedrocchi@stiima.cnr.it>
2022-03-08 10:42:30 +0100

    code coverage fix run tests

--- .coverage.sh
+++ .coverage.sh
@@ -8,8 +8,6 @@ cd $ws
 catkin build cnr_logger --no-deps --catkin-make-args run_tests 
 catkin build cnr_logger --no-deps --catkin-make-args coverage_report --cmake-args -DENABLE_COVERAGE_TESTING=ON -DCMAKE_BUILD_TYPE=Debug -DUSE_ROS=ON -DENABLE_TESTING=ON
 
-echo "Uploading coverage results to codecov.io"
-
 # Remove duplicated information
 rm "$ws/build/cnr_logger/coverage_report.info.cleaned"
 rm "$ws/build/cnr_logger/coverage_report.info.removed"
